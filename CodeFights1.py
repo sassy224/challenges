bf5ea84bcb536ee8876138468813b593092c75c7
Hans Nguyen <sassyboy224@gmail.com>
2015-07-19 21:48:30 +0700

    - Add solution for https://codefights.com/challenge/N6Lhzv6HEic2j4S4R and https://codefights.com/challenge/Y4c2swPy8RxtARAqn

---
+++ CodeFights1.py
@@ -0,0 +1,39 @@
+﻿"""
+Vanya gets bored one day and decides to enumerate a large pile of rocks. He first counts the rocks and finds out that he has n rocks in the pile, then he goes to the store to buy labels for enumeration. Each of the labels is a digit from 0 to 9 and each of the n rocks should be assigned a unique number from 1 to n.
+
+ 
+If each label costs $1, how much money will Vanya spend on this project?
+
+Input 1 (n) ? integer :
+integer n (1<=n<=10^9) the number of rocks in the pile.
+
+Output integer :
+the cost of the enumeration
+"""
+
+def rocks(n):
+	"""
+	>>> rocks(13)
+	17
+	>>> rocks(36123011)
+	277872985
+	
+	The trick here is to compute correctly the number of the digits from given n
+	E.g,
+	13 --> there're (13-9)*2 + 9 = 17 digits
+	100 --> there're (100-99)*3 + (99-9)*2 + 9
+	So you know the rule
+	"""
+	s = 0
+	l = len(str(n))
+	while l > 1:
+		# the trick here is magic of python: '9'*3 = '999'
+		s += (n - int('9'*(l-1))) * l
+		n = int('9'*(l-1))
+		l -= 1
+	return s + 9 # don't forget to plus 9 at the end
+	
+	
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
---
+++ CodeFights2.py
@@ -0,0 +1,43 @@
+﻿"""
+You are given a digital number written down on a sheet of paper. 
+Your task is to figure out if you rotate the given sheet of paper by 180 degrees would the number still look exactly the same.
+
+input: "1"  
+output: false
+
+input: "29562"  
+output: true
+
+input: "77"  
+output: false
+"""
+
+def digital_number(number):
+	"""
+	>>> digital_number('1')
+	False
+	>>> digital_number('29562')
+	True
+	>>> digital_number('77')
+	False
+	>>> digital_number('000')
+	True
+	
+	The trick here is you need to understand what rotation is:
+	https://en.wikipedia.org/wiki/Seven-segment_display_character_representations
+	In short:
+	0 -> 0
+	2 -> 2
+	5 -> 5
+	6 -> 9
+	8 -> 8
+	9 -> 6
+	Other numbers can't be rotated into another number
+	"""
+	rule = {'0':'0', '2':'2', '5':'5', '6':'9', '8':'8', '9':'6'}
+	new_str = [rule.get(digit, "x") for digit in number ]
+	return number == "".join(new_str[::-1])
+	
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
